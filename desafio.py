ac6a5fe5585d9fca7b2ce22fd5ce5daee5d32977
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2024-05-29 14:17:48 -0300

    feat: menu

---
+++ desafio.py
@@ -0,0 +1,34 @@
+saldo = 0
+extrado = ""
+limite_saque = 500
+numero_saque = 0
+LIMITE_SAQUE = 3
+
+menu = """
+----- MENU -----
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=>
+"""
+
+while True:
+  opcao = input(menu)
+
+  if opcao == "d":
+    pass
+  
+  elif opcao == "s":
+    pass
+
+  elif opcao == "e":
+    pass
+
+  elif opcao == "q":
+    break
+
+  else:
+    print("Opção inválida, por favor digitar umas das opções acima")
