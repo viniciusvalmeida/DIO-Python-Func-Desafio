0a56021599447c549911c53f2c7f667e1b3a2f68
Vinícius Almeida <81371812+viniciusvalmeida@users.noreply.github.com>
2024-05-29 14:47:37 -0300

    feat: Desafio completed

--- desafio.py
+++ desafio.py
@@ -1,5 +1,5 @@
 saldo = 0
-extrado = ""
+extrato = ""
 limite_saque = 500
 numero_saque = 0
 LIMITE_SAQUE = 3
@@ -12,20 +12,46 @@ menu = """
 [e] Extrato
 [q] Sair
 
-=>
-"""
+=> """
 
 while True:
   opcao = input(menu)
 
   if opcao == "d":
-    pass
+    valor = float(input("Digite o valor a ser depositado: "))
+
+    if valor > 0:
+      saldo += valor
+      extrato += f"\nDepósito: R$ {valor:.2f}\n"
+    
+    else:
+      print("Valor inválido")
   
   elif opcao == "s":
-    pass
+    valor = float(input("Digite o valor para saque: "))
+
+    if valor > saldo:
+      print("\nSaldo insuficiente")
+
+    elif valor > limite_saque:
+      print(f"\nLimite de saque no valor de R$ {limite_saque:.2f}")
+
+    elif numero_saque >= LIMITE_SAQUE:
+      print("\nNúmero de saques diários atingidos, tente novamente amanhã!")
+
+    elif valor > 0:
+      saldo -= valor
+      numero_saque += 1
+      extrato += f"\nSaque: R$ {valor:.2f}\n"
+
+    else:
+      print("Valor inválido")
 
   elif opcao == "e":
-    pass
+    print(" Extrato ".center(21, "-"))
+    print(extrato if extrato else "Não foram realizadas movimentações")
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("".center(21, "-"))
 
   elif opcao == "q":
     break
